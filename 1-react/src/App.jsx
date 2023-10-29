392adeea186ab769416908b85e1781e4d8891637
이소영 <th42500@naver.com>
2023-10-29 21:43:13 +0900

    feat: Button 컴포넌트 분리

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,3 +1,5 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="ProductPage">
@@ -12,7 +14,8 @@ function App() {
                 <div className="description">
                   <h2>고소한 바질 파스타</h2>
                   <div>6,000원</div>
-                  <button className="Button brand">주문하기</button>
+                  {/* <button className="Button brand">주문하기</button> */}
+                  <Button>주문하기</Button>
                 </div>
                 <div className="thumbnail">
                   <img
---
+++ 1-react/src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ children }) => (
+  <button className="Button brand">{children}</button>
+);
+
+export default Button;
