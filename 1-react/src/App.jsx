d7aff219bbe96fcfa53dc63f01a3f717db44a8dd
이소영 <th42500@naver.com>
2023-10-29 23:05:34 +0900

    feat: ProductPage 컴포넌트 분리

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,26 +1,28 @@
-import Title from "./components/Title";
-import ProductItem from "./components/ProductItem";
-import Navbar from "./components/Navbar";
-import Page from "./components/Page";
+// import Title from "./components/Title";
+// import ProductItem from "./components/ProductItem";
+// import Navbar from "./components/Navbar";
+// import Page from "./components/Page";
+import ProductPage from "./pages/ProductPage";
 
-const sample = {
-  id: "CACDA421",
-  name: "해물 계란 라면",
-  price: 6000,
-  thumbnail: "./images/menu-해물계란라면.jpg",
-};
+// const sample = {
+//   id: "CACDA421",
+//   name: "해물 계란 라면",
+//   price: 6000,
+//   thumbnail: "./images/menu-해물계란라면.jpg",
+// };
 
 function App() {
   return (
-    <div className="ProductPage">
-      <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
-        <ul>
-          <li>
-            <ProductItem product={sample} />
-          </li>
-        </ul>
-      </Page>
-    </div>
+    // <div className="ProductPage">
+    //   <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+    //     <ul>
+    //       <li>
+    //         <ProductItem product={sample} />
+    //       </li>
+    //     </ul>
+    //   </Page>
+    // </div>
+    <ProductPage />
   );
 }
 
