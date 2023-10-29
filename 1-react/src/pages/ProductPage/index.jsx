89d4dda4a6e84030d9d31118a759861685601024
이소영 <th42500@naver.com>
2023-10-29 23:25:47 +0900

    feat: ProductPage 클래스 생성 및 ProductApi 연동

--- 1-react/src/pages/ProductPage/index.jsx
+++ 1-react/src/pages/ProductPage/index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import ProductApi from "shared/api/ProductApi";
 import Navbar from "../../components/Navbar";
 import Page from "../../components/Page";
 import ProductItem from "../../components/ProductItem";
@@ -10,16 +12,49 @@ const sample = {
   thumbnail: "./images/menu-해물계란라면.jpg",
 };
 
-const ProductPage = () => (
-  <div className="ProductPage">
-    <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
-      <ul>
-        <li>
-          <ProductItem product={sample} />
-        </li>
-      </ul>
-    </Page>
-  </div>
-);
+// const ProductPage = () => (
+//   <div className="ProductPage">
+//     <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+//       <ul>
+//         <li>
+//           <ProductItem product={sample} />
+//         </li>
+//       </ul>
+//     </Page>
+//   </div>
+// );
+
+class ProductPage extends React.Component {
+  constructor(props) {
+    super(props); // React.Component의 생성자 호출
+
+    this.state = {
+      productList: [], // 상품을 데이터를 받아올 빈 배열
+    };
+  }
+
+  async fetch() {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      this.setState({ productList }); // 상태 갱신
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="ProductPage">
+        <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+          <ul>
+            <li>
+              <ProductItem product={sample} />
+            </li>
+          </ul>
+        </Page>
+      </div>
+    );
+  }
+}
 
 export default ProductPage;
