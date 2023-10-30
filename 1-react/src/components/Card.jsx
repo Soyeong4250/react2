63eda90586c2ec41fa1bab642aea0cca3c56ac1c
이소영 <th42500@naver.com>
2023-10-30 12:24:16 +0900

    feat: Card 컴포넌트 분리

---
+++ 1-react/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => (
+  <div className="Card">
+    <header>헤더</header>
+    <main>본문</main>
+    <footer>푸터</footer>
+  </div>
+);
+
+export default Card;
--- 1-react/src/pages/OrderPage/index.jsx
+++ 1-react/src/pages/OrderPage/index.jsx
@@ -1,11 +1,13 @@
 import Page from "../../components/Page";
 import Navbar from "../../components/Navbar";
 import Title from "../../components/Title";
+import Card from "../../components/Card";
 
 const OrderPage = () => (
   <div className="OrderPage">
     <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
-    컨텐츠 나오는 곳
+    {/* 컨텐츠 나오는 곳 */}
+    <Card />
     </ Page>
   </div>
 );
