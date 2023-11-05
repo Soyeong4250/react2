1fd02e60415d926574d417be503c39466018aea1
이소영 <th42500@naver.com>
2023-11-05 20:27:24 +0900

    feat: ProductApi 연동

--- 1-react/src/pages/CartPage/index.jsx
+++ 1-react/src/pages/CartPage/index.jsx
@@ -1,31 +1,51 @@
-// import Button from "../../components/Button";
+import React from 'react';
+import ProductApi from '../../../../shared/api/ProductApi';
 import PaymentButton from "../CartPage/PaymentButton"
 import Page from "../../components/Page";
 import Title from "../../components/Title";
 import ProductItem from './../../components/ProductItem';
-// import FormControl from "../../components/FormControl";
 import OrderForm from './OrderForm';
 
-const sample =  {
-  id: "CACDA421",
-  name: "해물 계란 라면",
-  price: 6000,
-  thumbnail: "./images/menu-해물계란라면.jpg"
-};
+// const sample =  {
+//   id: "CACDA421",
+//   name: "해물 계란 라면",
+//   price: 6000,
+//   thumbnail: "./images/menu-해물계란라면.jpg"
+// };
 
-const CartPage = () => (
-  <div className="CartPage">
-      <Page 
-      header={<Title backUrl={"/"}>장바구니</Title>} 
-      footer={<PaymentButton />}>
-        <ProductItem product={sample}/>
-      </Page>
-      {/* <FormControl label="이름" htmlFor={'name'} required>
-        <input type="text" id="name" />
-      </FormControl> */}
+class CartPage extends React.Component {
 
-      <OrderForm />
-  </div>
-);
+  constructor(props) {
+    super(props);
+    this.state = { product: null };
+  }
+
+  async fetch() {
+    try {
+      const product = await ProductApi.fetchProduct("CACDA423");
+      this.setState({ product });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  render() {
+    const { product } = this.state;
+
+    return(<div className="CartPage">
+        <Page 
+        header={<Title backUrl={"/"}>장바구니</Title>} 
+        footer={<PaymentButton />}>
+          {product && <ProductItem product={ product } />}
+        </Page>
+
+        <OrderForm />
+    </div>);
+  }
+}
 
 export default CartPage;
