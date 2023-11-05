8e4e919a5e147d0c34e276d53e8fdc70cfffc76e
이소영 <th42500@naver.com>
2023-11-05 18:52:47 +0900

    feat: divElement를 이용한 Ref test

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -8,4 +8,29 @@ function App() {
   return <CartPage />
 }
 
-export default App;
+// export default App;
+
+import React from "react";
+class MyComponent extends React.Component {  // ref 객체는 지속적으로 관리할 수 있도록 저장해야 하므로 class로 생성
+  divRef = React.createRef();  // ref 객체 생성
+
+  constructor(props) {
+    super(props)
+    console.log('constructor', this.divRef);
+  }
+
+  render() {
+    console.log('render', this.divRef);
+    return <div ref={this.divRef}>div</div>
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount', this.divRef);
+    const divElement = this.divRef.current;
+    divElement.style.backgroundColor = 'red';
+    divElement.style.height = '100px';
+    divElement.style.width =  '100px';
+  }
+}
+
+export default MyComponent;
