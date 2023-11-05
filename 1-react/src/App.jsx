e78a43110016962faa35777e2f0dafe0e4952e86
이소영 <th42500@naver.com>
2023-11-05 19:38:14 +0900

    feat: Ref 객체 연동을 통한 주문폼 구현

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -8,40 +8,4 @@ function App() {
   return <CartPage />
 }
 
-// export default App;
-
-import React from "react";
-class Foo extends React.Component {
-  render() {
-    return <>Foo</>
-  }
-}
-
-class MyComponent extends React.Component {  // ref 객체는 지속적으로 관리할 수 있도록 저장해야 하므로 class로 생성
-  divRef = React.createRef();  // ref 객체 생성
-  fooRef = React.createRef();
-
-  constructor(props) {
-    super(props)
-    console.log('constructor', this.divRef);
-  }
-
-  render() {
-    console.log('render', this.divRef);
-    return <>
-      <div ref={this.divRef}>div</div>
-      <Foo ref={this.fooRef }/>
-    </>
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount divRef', this.divRef);
-    console.log('componentDidMount fooRef', this.fooRef);
-    const divElement = this.divRef.current;
-    divElement.style.backgroundColor = 'red';
-    divElement.style.height = '100px';
-    divElement.style.width =  '100px';
-  }
-}
-
-export default MyComponent;
+export default App;
--- 1-react/src/pages/CartPage/OrderForm.jsx
+++ 1-react/src/pages/CartPage/OrderForm.jsx
@@ -1,34 +1,68 @@
+import React from 'react';
 import FormControl from '../../components/FormControl';
 
-const OrderForm = () => {
+// const OrderForm = () => {
 
-    const handleSubmit = e => {
-        e.preventDefault();  // 서버로 다시 요청하는 기본 동작을 막기 위해 사용
-        console.log("submit");
+class OrderForm extends React.Component {  // 계속 유지되는 값을 만들기 위해 class 사용
+    constructor(props) {
+        super(props)
+
+        this.formRef = React.createRef();
+        this.handleSubmit = this.handleSubmit.bind(this)  // handleSubmit이 비동기로 동작하므로 현재 인스턴스를 this로 고정
     }
 
-    return (
-        <form className='OrderForm' id='order-form' onSubmit={handleSubmit}>
-            <FormControl label="주소" htmlFor={'deliveryAddress'} required>
-                <input type="text" name='deliveryAddress' id='deliveryAddress' placeHolder='배달받을 주소를 입력하세요.' required autoFocus/>
-            </FormControl>
-            <FormControl label="연락처" htmlFor={'deliveryContact'} required>
-                <input type="text" name='deliveryContact' id='deliveryContact' placeHolder='연락처를 입력하세요.' required pattern='^\d{2,3}-\d{3,4}-\d{4}$'/>
-            </FormControl>
-            <FormControl label="결제수단" htmlFor={'paymentMethod'} required>
-                <select name='paymentMethod' id='paymentMethod' value=''>
-                    <option value="마이페이">마이페이</option>
-                    <option value="만나서 결제">만나서 결제</option>
-                </select>
-            </FormControl>
-            <FormControl label="가게 사장님께" htmlFor={'messageToShop'}>
-                <textarea name='messageToShop' id='messageToShop'/>
-            </FormControl>
-            <FormControl label="라이더님께" htmlFor={'messageToRider'}>
-                <textarea name='messageToRider' id='messageToRider'/>
-            </FormControl>
-        </form>
-    );
+    getInputValueByName(name) {
+        if(!this.formRef.current) return;  // 렌더링 되기 전에는 formRef가 null 일 수 있으므로 return
+        console.log('formRef', this.formRef.current);
+
+        const inputElement = this.formRef.current.elements.namedItem(name)
+        if(!inputElement) return '';
+
+        return inputElement.value
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();  // 서버로 다시 요청하는 기본 동작을 막기 위해 사용
+        
+        const deliveryAddress = this.getInputValueByName('deliveryAddress')
+        const deliveryContact = this.getInputValueByName('deliveryContact')
+        const paymentMethod = this.getInputValueByName('paymentMethod')
+        const messageToShop = this.getInputValueByName('messageToShop')
+        const messageToRider = this.getInputValueByName('messageToRider')
+        
+        console.log("submit", this.formRef.current, {
+            deliveryAddress,
+            deliveryContact,
+            paymentMethod,
+            messageToShop,
+            messageToRider
+        });
+    }
+
+    render() {
+        return (
+            <form className='OrderForm' id='order-form' ref={this.formRef} onSubmit={this.handleSubmit}>
+                <FormControl label="주소" htmlFor={'deliveryAddress'} required>
+                    <input type="text" name='deliveryAddress' id='deliveryAddress' placeholder='배달받을 주소를 입력하세요.' required autoFocus/>
+                </FormControl>
+                <FormControl label="연락처" htmlFor={'deliveryContact'} required>
+                    <input type="text" name='deliveryContact' id='deliveryContact' placeholder='연락처를 입력하세요.' required pattern='^\d{2,3}-\d{3,4}-\d{4}$'/>
+                </FormControl>
+                <FormControl label="결제수단" htmlFor={'paymentMethod'} required>
+                    <select name='paymentMethod' id='paymentMethod'>
+                        <option value="마이페이">마이페이</option>
+                        <option value="만나서 결제">만나서 결제</option>
+                    </select>
+                </FormControl>
+                <FormControl label="가게 사장님께" htmlFor={'messageToShop'}>
+                    <textarea name='messageToShop' id='messageToShop'/>
+                </FormControl>
+                <FormControl label="라이더님께" htmlFor={'messageToRider'}>
+                    <textarea name='messageToRider' id='messageToRider'/>
+                </FormControl>
+            </form>
+        );
+    }
 }
 
 export default OrderForm
