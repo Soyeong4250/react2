4d38f3e3e7c833cd7134aef92d2bc0990a0eea81
이소영 <th42500@naver.com>
2023-11-06 10:42:11 +0900

    feat: EventEmitter 실습

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,5 +1,6 @@
 // import ProductPage from "./pages/ProductPage";
 // import OrderPage from "./pages/OrderPage";
+import { createEventEmitter } from "../../shared/lib/EventEmitter";
 import CartPage from "./pages/CartPage";
 
 function App() {
@@ -9,3 +10,13 @@ function App() {
 }
 
 export default App;
+
+const eventEmitter = createEventEmitter(0);
+const logger = value => console.log(value);
+
+eventEmitter.on(logger);  // App.jsx에 이벤트가 들어오면 logging
+console.log(eventEmitter.get());  // 0
+eventEmitter.set(1);  // 1
+eventEmitter.set(2);  // 2
+
+setTimeout(() => eventEmitter.set(10), 3000);  // 비동기로 3초후에 10 출력
--- shared/lib/EventEmitter.js
+++ shared/lib/EventEmitter.js
@@ -1,15 +1,15 @@
 export const createEventEmitter = (value) => {
   let handlers = [];
 
-  const on = (handler) => handlers.push(handler);
-  const off = (handler) => {
+  const on = (handler) => handlers.push(handler);  // 구독자(수신자)를 추가하는 함수
+  const off = (handler) => {  // 구독자(수신자)를 제거하는 함수
     handlers = handlers.filter((h) => h !== handler);
   };
 
-  const get = () => value;
+  const get = () => value;  // closer 역할
   const set = (newValue) => {
     value = newValue;
-    handlers.forEach((handler) => handler(value));
+    handlers.forEach((handler) => handler(value));  // 각 구독객체에게 변경된 값을 통지
   };
 
   return {
